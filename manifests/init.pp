11f4bf64f17a444e1c0c94691753b659698a88ca
Chris Braddock <braddock.chris@gmail.com>
2014-10-24 09:51:19 -0400

    corrected provider to appzip

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,7 +2,7 @@ class puppet-beyondcompare(
   $version = '4.0.0.18847'
 ) {
   package { 'Beyond Compare':
-    provider => 'appdmg',
+    provider => 'appzip',
     source   => "http://www.scootersoftware.com/BCompareOSX-$version.zip
   }
 }
