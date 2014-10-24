f2cb1f0dba33a709c3f306b77b0cb09a7fc5cc47
Chris Braddock <braddock.chris@gmail.com>
2014-10-24 09:59:27 -0400

    try compressed_app provider

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,7 +2,7 @@ class beyondcompare(
   $version = '4.0.0.18847'
 ) {
   package { 'Beyond Compare':
-    provider => 'appzip',
+    provider => 'compressed_app',
     source   => "http://www.scootersoftware.com/BCompareOSX-$version.zip"
   }
 }
