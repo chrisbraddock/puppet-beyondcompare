44cf48640479463e961ae90aa291a70d8b893a14
Chris Braddock <braddock.chris@gmail.com>
2014-10-24 09:47:46 -0400

    initial commit

---
+++ manifests/init.pp
@@ -0,0 +1,9 @@
+class puppet-beyondcompare(
+  $version = '4.0.0.18847'
+) {
+  package { 'Beyond Compare':
+    provider => 'appdmg',
+    source   => "http://www.scootersoftware.com/BCompareOSX-$version.zip
+  }
+}
+
